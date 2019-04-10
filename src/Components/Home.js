574996b860c9132e8195e7a7a54e4e207d1ed676
EstellePieragnolo <estellepieragnolo@gmail.com>
2019-04-10 23:01:50 +0200

    add shopintro items

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Logo from './Logo';
 import './_Home.scss';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
 export default class Home extends React.Component {
 
@@ -8,14 +10,54 @@ export default class Home extends React.Component {
     return (
       <div className="home">
         <div className="homeHeader">
-          <div className="homeHeaderLogo"><Logo /></div>
-          <h1 className="homeHeaderTitle">LA ROUTE DE LA PIERRE</h1>
+          <div className="homeHeaderItems">
+            <div className="homeHeaderItemsLogo"><Logo /></div>
+            <h1 className="homeHeaderItemsTitle">LA ROUTE DE LA PIERRE</h1>
+          </div>
+          <div className="homeHeaderSubtitle">
+            <h3>Créations en taille pierre </h3>
+            <h3>par Orianne & Louis</h3>
+          </div>
         </div>
-        <div className="homeFooter">
-          <h3>Créations en taille pierre </h3>
-          <h3>par Orianne & Louis</h3>
+        <div className="homeShop">
+          <h2 className="homeShopTitle">Le magasin</h2>
+          <div className="homeShopItems">
+            <Query query={items}>
+              {({ data, loading, error }) => {
+                if (loading) return 'Loading';
+                if (error) return <p>ERROR</p>;
+                return (
+                  data.introShops &&
+                  data.introShops.map(introShop => {
+                    return <div key={introShop.id}>
+                      {
+                        introShop.image &&
+                        <img src={introShop.image.url} alt="shopCategories" width="360" />
+                      }
+                      <div className="homeShopItemsHover">
+                        <p>{introShop.category}</p>
+                      </div>
+                    </div>
+                  })
+
+                );
+              }}
+            </Query>
+          </div>
         </div>
       </div >
     )
   }
 }
+
+export const items = gql`
+query {
+  introShops {
+    id,
+    category,
+    image {
+      url
+    }
+  }
+}
+`
