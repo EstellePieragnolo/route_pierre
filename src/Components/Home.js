e00e20867578674bfce49ab6744ba0f9a2bb9853
EstellePieragnolo <estellepieragnolo@gmail.com>
2019-05-05 19:04:07 +0200

    home fix position pb and add logo background

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,24 +9,21 @@ import Header from './Header/Header';
 export default class Home extends React.Component {
 
   render() {
-
+    console.log(this.back)
     return (
       <div className="home">
-        <div className={"homeNavbar" + (
-          document.documentElement.scrollTop > window.innerHeight - 40 ? 'top' : 'down'
-        )}>
+        <div className="homeNavbar">
           <Header />
         </div>
         <div className="homeHeader">
-          <div className="homeHeaderItems">
-            <div className="homeHeaderItemsLogo">
-              <Logo height={'390px'} />
+          <div className="homeHeaderContainer">
+            <div className="homeHeaderContainerItems">
+              <Logo height={'390px'} image="title" />
             </div>
-            <h1 className="homeHeaderItemsTitle">LA ROUTE DE LA PIERRE</h1>
-          </div>
-          <div className="homeHeaderSubtitle">
-            <h3>Créations en taille pierre </h3>
-            <h3>par Orianne & Louis</h3>
+            <div className="homeHeaderContainerSubtitle">
+              <h3>Créations en taille pierre </h3>
+              <h3>par Orianne & Louis</h3>
+            </div>
           </div>
         </div>
         <div className="homeShop">
--- src/Components/Logos/Logo.js
+++ src/Components/Logos/Logo.js
@@ -6,28 +6,27 @@ export default class Logo extends React.Component {
 
   render() {
     return (
-      <div>
-        <Query query={logo}>
-          {({ data, loading, error }) => {
-            if (loading) return null;
-            if (error) return null;
+      <Query query={logo}>
+        {({ data, loading, error }) => {
+          if (loading) return null;
+          if (error) return null;
 
-            return (
-              data.logoes &&
-              data.logoes.map(logos => {
-                if (logos.title === 'mainLogo') {
-                  return (
-                    logos.logo &&
-                    <img key={logos.id} src={logos.logo.url} alt="logo" height={this.props.height} />
-                  )
-                } else return null;
-              })
+          return (
+            data.logoes &&
+            data.logoes.map(logos => {
+              if (logos.title === 'mainLogo') {
 
-            );
-          }}
-        </Query>
+                if (logos.logo && this.props.image === 'img') {
+                  return <img key={logos.id} src={logos.logo.url} alt="logo" height={this.props.height} />
+                } else if (logos.logo && this.props.image === 'title') {
+                  return <h1 className="homeHeaderContainerItemsTitle" style={{ backgroundImage: `url(${logos.logo.url})` }}>LA ROUTE DE LA PIERRE</h1>
+                } else return null
+              } else return null;
+            })
 
-      </div>
+          );
+        }}
+      </Query>
     )
   }
 }
