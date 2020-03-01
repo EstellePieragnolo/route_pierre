b73806fdc254ffa9c2fabe501c7be4a849f4465e
Estelle <estellepieragnolo@gmail.com>
2020-03-01 18:50:20 +0000

    update home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
-import Inspirations from "./Inspirations";
-import ShopCategories from "./Shop/ShopCategories";
-import PreFooter from "./Footers/PreFooter";
 import "./_Home.scss";
 import Header from "./Header/Header";
 import logo from "../img/logo.svg";
+import Text from "./Text/Text";
+import Title from "./Title/Title";
 
 export default class Home extends React.Component {
   render() {
+    const text =
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullam- corper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in";
     return (
       <div className="home">
         <div className="homeNavbar">
@@ -20,16 +21,18 @@ export default class Home extends React.Component {
               <h1>LA ROUTE DE LA PIERRE</h1>
             </div>
             <div className="homeHeaderContainerSubtitle">
-              <h3>Créations en taille pierre <br/> par Orianne <span>&</span> Louis</h3>
+              <h3>
+                Créations en taille pierre <br /> par Orianne <span>&</span>{" "}
+                Louis
+              </h3>
             </div>
           </div>
         </div>
         <div className="homeShop">
-          <h2 className="homeShopTitle">Le magasin</h2>
-          <h3>Nos créations</h3>
-          <ShopCategories />
-          <Inspirations />
-          <PreFooter />
+          <div>
+          <Title title="Le Project" />
+          <Text text={text} />
+          </div>
         </div>
       </div>
     );
